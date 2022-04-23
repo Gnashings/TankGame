60af66b643c8a2c8e0cff1f252a31e13ff7248c0
Nash Roy <nashroy11@gmail.com>
2022-04-23 16:20:37 -0700

    Update enemyBomberFollow.cs

--- Assets/enemyBomberFollow.cs
+++ Assets/enemyBomberFollow.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class enemyBomberFollow : MonoBehaviour
+public class EnemyBomberFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
-    public Transform Player;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
-          if (Player == null)
-            Player = GameObject.FindGameObjectWithTag("Player").transform;
+          if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(Player.position);
+        enemy.SetDestination(player.position);
     }
 }
