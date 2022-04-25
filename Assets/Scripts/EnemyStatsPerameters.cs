3403bc4a1474f39098830549ec0af7ce91235f35
Nash Roy <nashroy11@gmail.com>
2022-04-25 15:30:56 -0700

    Update EnemyStatsPerameters.cs

--- Assets/Scripts/EnemyStatsPerameters.cs
+++ Assets/Scripts/EnemyStatsPerameters.cs
@@ -8,6 +8,7 @@ public class EnemyStatsPerameters : ScriptableObject
     public string enemyName;
     public float health;
     public bool canMove;
+    [Header("Gun Stats")]
     public float fireRate;
     public float damage;
     public float bulletVelocity;
