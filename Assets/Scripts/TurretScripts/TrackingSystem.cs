9ec9359808b8915b6a8699d82232983b799c328b
Nash Roy <nashroy11@gmail.com>
2022-04-26 15:41:27 -0700

    enemy fixes

--- Assets/Scripts/TurretScripts/TrackingSystem.cs
+++ Assets/Scripts/TurretScripts/TrackingSystem.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TrackingSystem : MonoBehaviour {
     [Tooltip("How fast do you want enemy to track onto you")]
     public float spinSpeed = 3.0f;
- 
+    public bool lookUpDown;
     [Tooltip("What is it tracking")]
     private GameObject target;
     
@@ -26,9 +26,15 @@ public class TrackingSystem : MonoBehaviour {
     void Update () {
         if(target){
             if(m_lastKnownPosition != target.transform.position){
-                m_lastKnownPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+                if(lookUpDown)
+                {
+                    m_lastKnownPosition = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+                }
+                else
+                    m_lastKnownPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+
                 m_lookAtRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position);
-               // m_lookAtRotation.x = 0f;
+                //m_lookAtRotation.x = 0f;
             }
  
             if(transform.rotation != m_lookAtRotation){
